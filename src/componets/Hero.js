490a206a5528edb4d2db9f388bdf598c6f91f533
kbax6342 <k3vin.baxt3r@gmail.com>
2023-01-07 01:43:23 -0500

    changed background

--- src/componets/Hero.js
+++ src/componets/Hero.js
@@ -8,10 +8,13 @@ export default function Hero() {
     return (
         <>
             <div className='relative'>
-                <video autoPlay loop muted id='video' className='object-cover w-full max-h-25 '>
+                {/* <video autoPlay loop muted id='video' className='object-cover w-full max-h-25 '>
                     <source src={`https://streamable.com/yrbmlo`} type='video/mp4' /> 
                    
-                </video>
+                </video> */}
+                <div className='relative object-cover w-full mx-auto'>
+                    <img src={`https://images.pexels.com/photos/1330219/pexels-photo-1330219.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`} alt=""  className='object-cover w-full'/>
+                </div>
                 <div className='absolute top-5 right-5 w-full hidden md:hidden lg:block'>
                         <img src={img2} alt="genesis trc"  className='mx-auto '/>
                     <Coming />
