7e93428005eea76cf848c9fc610ddd654ace7263
kbax6342 <k3vin.baxt3r@gmail.com>
2023-01-07 01:34:32 -0500

    adding videos

--- src/componets/Hero.js
+++ src/componets/Hero.js
@@ -9,7 +9,7 @@ export default function Hero() {
         <>
             <div className='relative'>
                 <video autoPlay loop muted id='video' className='object-cover w-full max-h-25 '>
-                    <source src={video} type='video/mp4' /> 
+                    <source src={`https://streamable.com/yrbmlo`} type='video/mp4' /> 
                    
                 </video>
                 <div className='absolute top-5 right-5 w-full hidden md:hidden lg:block'>
@@ -22,18 +22,7 @@ export default function Hero() {
                 </div>
             </div>
 
-            {/* <article className='relative'>
-        {stories[0].mainImage  &&
-           <img src={stories[0].mainImage.asset.url} alt="" className='h-96 w-full object-cover rounded-2xl'/>
-        }
-         <div className='absolute bottom-8 left-8'>
-         <h1 className='text-4xl lg:text-5xl mb-6 text-white'>{stories[0].title}</h1>
-         <p className='text-slate-100 mb-8 md:w-1/2 '>{stories[0].body[0].children[0].text.substring(0, 200)} </p>
-         <button className='bg-white py-2 px-8 rounded shadow text-slate-800 tracking-wide hover:opacity-75 transtion-all duration-200'>
-           Read More
-           </button>
-         </div>
-       </article> */}
+        
         </>
     )
 }
